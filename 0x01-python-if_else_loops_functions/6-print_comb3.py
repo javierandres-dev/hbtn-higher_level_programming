96cc3202db758859e5a6146636a1bac6192af25f
1270holberton <1270@holbertonschool.com>
2019-12-11 22:02:53 +0000

    task6fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i < j and i == 8:
             print('{:d}{:d}'.format(i, j))
-        else:
+        if i < j and i != 8:
             print('{:d}{:d}, '.format(i, j), end='')
