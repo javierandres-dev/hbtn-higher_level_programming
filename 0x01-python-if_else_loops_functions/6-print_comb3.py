5fa2a6654130c850a60258e8ef0b95c08d1ece23
1270holberton <1270@holbertonschool.com>
2019-12-11 21:59:58 +0000

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j and i == 8:
+            print('{:d}{:d}'.format(i, j))
+        else:
+            print('{:d}{:d}, '.format(i, j), end='')
