834da2226b338434c8733f505d7b067addd89fe5
1270holberton <1270@holbertonschool.com>
2019-12-11 22:44:20 +0000

    task9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number < 0):
+        x = number % -10 * -1
+    else:
+        x = number % 10
+    print("{:d}".format(x), end="")
+    return (x)
