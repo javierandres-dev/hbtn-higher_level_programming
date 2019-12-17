7517df5c7b6e10b25a1a7fa37493ce29a6b37703
1270holberton <1270@holbertonschool.com>
2019-12-17 14:59:40 -0500

    task1fix2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx > len(my_list):
         return None
-    if idx > len(my_list):
-        return None
-    for i in my_list:
-        if i == idx:
-            return my_list[i]
+    return (my_list[idx])
