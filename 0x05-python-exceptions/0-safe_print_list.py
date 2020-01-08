83ef8472d0c0fa2d0ae181e4c7aac11d4d209ef4
1270holberton <1270@holbertonschool.com>
2020-01-08 15:39:11 -0500

    task0fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    amount = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-            i+= 1
+            amount += 1
         except:
             print('')
-            return i
+            return amount
     print('')
-    return i
+    return amount
