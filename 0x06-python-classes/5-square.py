936440fd77c715fd2afe1b1eb18abf61439eb024
1270holberton <1270@holbertonschool.com>
2020-01-10 13:57:56 -0500

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+            self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        result = self.__size * self.__size
+        return result
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
