e03eaf6b7c14914284e34eb0e54d41d89d7d3b04
1270holberton <1270@holbertonschool.com>
2020-01-13 16:53:27 -0500

    update with pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@ def matrix_divided(matrix, div):
     size = len(matrix[0])
     for i in range(len(matrix)):
         if size != len(matrix[i]):
-            raise TypeError("Each row of the matrix must have the same size") 
+            raise TypeError("Each row of the matrix must have the same size")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
@@ -13,7 +13,8 @@ def matrix_divided(matrix, div):
     for j in matrix:
         for k in j:
             if type(k) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) \
+                of integers/floats")
         tmp = list(map(lambda x: round(x / div, 2), j))
         new_matrix.append(tmp)
     return new_matrix
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,6 @@
 def text_indentation(text):
-    """ Function that prints a text with 2 new lines after each of these characters: ., ? and : """
+    """ Function that prints a text with 2 new lines \
+    after each of these characters: ., ? and : """
     if type(text) == str:
         text = text.replace('. ', '.\n\n')
         text = text.replace('? ', '?\n\n')
