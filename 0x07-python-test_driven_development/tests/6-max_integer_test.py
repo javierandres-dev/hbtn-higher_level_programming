604566bee984bb36713c23391771fd6c206b928a
1270holberton <1270@holbertonschool.com>
2020-01-13 18:28:32 -0500

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+    Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """ methods for testing the function 'max_integer()' """
+    def test_greater(self):
+        """ find greater  """
+        self.assertAlmostEqual(max_integer([-2, -1, 0, 1, 2, 3, 4]), 4)
+
+    def test_empty(self):
+        """ find empty  """
+        self.assertAlmostEqual(max_integer(), None)
+
+    def test_null(self):
+        """ find null  """
+        self.assertAlmostEqual(max_integer([None]), None)
