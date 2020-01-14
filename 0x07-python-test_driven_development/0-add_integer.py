5f8cc81982d0d968df8145192355fb6f211b780e
1270holberton <1270@holbertonschool.com>
2020-01-14 18:34:23 -0500

    task0fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """ Function that adds 2 integers. """
-    try:
-        return int(a) + int(b)
-    except:
-        if type(a) not in [int, float]:
-            raise TypeError("a must be an integer")
-        elif type(b) not in [int, float]:
-            raise TypeError("b must be an integer")
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    elif type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    else:
+        if abs(a + b) == float('inf'):
+            raise OverflowError('overflow error')
+        else:
+            return int(a) + int(b)
