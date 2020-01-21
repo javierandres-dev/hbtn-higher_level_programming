13b7eb381f62a65d8d2f254d4c272177841e3ac6
1270holberton <1270@holbertonschool.com>
2020-01-21 17:37:59 -0500

    task5

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def to_json_string(my_obj):
+    """
+        function that returns the JSON representation of an object (string)
+    """
+    import json
+    return json.dumps(my_obj)
