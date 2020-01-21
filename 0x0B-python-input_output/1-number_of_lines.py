69b22c3be6a53053e26696e56de9d742124ab11c
1270holberton <1270@holbertonschool.com>
2020-01-21 15:43:27 -0500

    task1

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+    """  function that returns the number of lines of a text file """
+    with open(filename) as f:
+        return (len(f.readlines()))
