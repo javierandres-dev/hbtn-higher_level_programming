cb64f57d56209cdd70bb2a0fb3413355bfc5c280
1270holberton <1270@holbertonschool.com>
2020-01-21 23:43:00 -0500

    task9fix1

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+from sys import argv
 """
     script that adds all arguments to a Python list,
     and then save them to a file
 """
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-from sys import argv
 my_list = []
 try:
     my_load = load_from_json_file('add_item.json')
