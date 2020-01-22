a55e92acded5f5a75157009cc0de51adaf6c54d0
1270holberton <1270@holbertonschool.com>
2020-01-21 23:39:00 -0500

    task9fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,11 +5,14 @@
 """
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-import sys
+from sys import argv
+my_list = []
 try:
-    pl = load_from_json_file('add_item.json')
+    my_load = load_from_json_file('add_item.json')
+    for i in my_load:
+        my_list.append(i)
 except:
-    pl = []
-for i in range(1, len(sys.argv)):
-    pl.append(sys.argv[i])
-save_to_json_file(pl, "add_item.json")
+    my_list = []
+for j in range(1, len(argv)):
+    my_list.append(argv[j])
+save_to_json_file(my_list, 'add_item.json')
