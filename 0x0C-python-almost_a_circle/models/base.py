eda1361384be7cdb624d98c571ccc9104e6964e5
1270holberton <1270@holbertonschool.com>
2020-01-24 09:39:58 -0500

    task1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Base:
+    """
+        First class.  This class will be the “base” of all other classes in
+        this project. The goal of it is to manage id attribute in all your
+        future classes and to avoid duplicating the same code
+        (by extension, same bugs)
+    """
+    __nb_objects = 0
+    """ private class attribute """
+
+    def __init__(self, id=None):
+        """ class constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
