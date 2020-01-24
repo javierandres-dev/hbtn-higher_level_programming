4e084753ea816aca3ac4f0f50ecac8b8895b2cf2
1270holberton <1270@holbertonschool.com>
2020-01-24 14:33:35 -0500

    task11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,18 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         """ attribute access """
 
+    @property
+    def size(self):
+        """ getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ setter """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ overriding the __str__ method """
         return ('[Square] ({:d}) {:d}/{:d} - {:d}'
-                .format(self.id, self.x, self.y, self.width))
+                .format(self.id, self.x, self.y, self.size))
