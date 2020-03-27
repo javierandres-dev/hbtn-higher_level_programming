196c27992de967d1eaa56cfeda4f1b7e7547f23c
0x00developer <info@tecnoayuda.co>
2020-03-27 22:54:29 +0000

    task0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" shebang line - defines where the interpreter is located """
+""" script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+""" module to connect to a MySQL database and execute SQL queries. """
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    passwd="admin",
+    db="hbtn_0e_0_usa",
+    charset="utf8"
+)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
