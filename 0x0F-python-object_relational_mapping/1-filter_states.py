1c77bfafca00e743ccbf6574d73b3eb8130272df
0x00developer <info@tecnoayuda.co>
2020-03-28 23:34:17 +0000

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
-"""
-    script that lists all states with a name starting with N
-    (upper N) from the database hbtn_0e_0_usa
-"""
+""" script that lists all states from the database hbtn_0e_0_usa """
 from sys import argv
-""" module to receive arguments """
 import MySQLdb
-""" module to connect to a MySQL database and execute SQL queries. """
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    charset="utf8"
-)
-cur = conn.cursor()
-try:
-    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC;")
-    query_rows = cur.fetchall()
-except MySQLdb.Error:
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
     try:
-        print ("MySQLdb Error")
-    except IndexError:
-        print ("MySQLdb Error - IndexError")
-for row in query_rows:
-    print(row)
-cur.close()
-""" Close all cursors """
-conn.close()
-""" Close all databases """
+        stmt = "SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
+    cur.close()
+    conn.close()
