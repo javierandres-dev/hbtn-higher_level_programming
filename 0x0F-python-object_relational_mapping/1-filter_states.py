14b721f9df844d6da5443e75b4b936e40844088f
0x00developer <info@tecnoayuda.co>
2020-03-28 23:47:05 +0000

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
         charset="utf8")
     cur = conn.cursor()
     try:
-        stmt = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        stmt = """
+        SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC
+        """
         cur.execute(stmt)
         rtn = cur.fetchall()
     except MySQLdb.Error:
