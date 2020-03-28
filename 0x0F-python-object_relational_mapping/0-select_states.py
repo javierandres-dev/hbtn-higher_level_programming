5c82d3574be93ca0a0c9ab0e1c46fd8a843710c3
0x00developer <info@tecnoayuda.co>
2020-03-28 22:26:15 +0000

    Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """ shebang line - defines where the interpreter is located """
 """ script that lists all states from the database hbtn_0e_0_usa """
-import sys
 from sys import argv
 """ module to receive arguments """
 import MySQLdb
 """ module to connect to a MySQL database and execute SQL statements """
 if __name__ == "__main__":
     """ execute only if run as a sript """
-    n_arg = len(sys.argv)
-    if n_arg <= 1:
-        print ("Arguments are missing")
-        sys.exit(1)
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -26,15 +21,13 @@ if __name__ == "__main__":
         SELECT * FROM states;
         """
         cur.execute(stmt)
-        query_rows = cur.fetchall()
+        rtn = cur.fetchall()
     except MySQLdb.Error:
         try:
             print ("MySQLdb Error")
-            sys.exit(1)
         except IndexError:
             print ("MySQLdb Error - IndexError")
-            sys.exit(1)
-    for row in query_rows:
+    for row in rtn:
         print(row)
     cur.close()
     """ Close all cursors """
