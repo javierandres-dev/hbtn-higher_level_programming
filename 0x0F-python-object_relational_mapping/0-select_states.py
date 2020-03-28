46a6fbcedcae3ba888e3ed8892b94f0d77251e5b
0x00developer <info@tecnoayuda.co>
2020-03-28 23:06:34 +0000

    Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" shebang line - defines where the interpreter is located """
 """ script that lists all states from the database hbtn_0e_0_usa """
 from sys import argv
+""" module to use command line arguments """
 import MySQLdb
+""" module to connect to a MySQL database and execute SQL queries """
 if __name__ == "__main__":
+    """ execute only if run as a sript, not be executed when imported """
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -16,3 +20,4 @@ if __name__ == "__main__":
     for i in cur:
         print(i)
     conn.close()
+    """ Close connection """
