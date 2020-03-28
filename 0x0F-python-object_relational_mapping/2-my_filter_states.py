621173e35a326a57b29a1eb3319b6c59ee7ce167
0x00developer <info@tecnoayuda.co>
2020-03-28 23:57:09 +0000

    task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
 """
-    script that takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa where name matches the argument.
+    script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
 """
 from sys import argv
-""" module to use command line arguments """
 import MySQLdb
-""" module to connect to a MySQL database and execute SQL queries. """
 if __name__ == "__main__":
-    """ execute only if run as a sript """
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3],
-        charset="utf8"
-    )
+        charset="utf8")
     cur = conn.cursor()
     try:
         search = argv[4]
@@ -25,15 +20,13 @@ if __name__ == "__main__":
         SELECT * FROM states WHERE name='{:s}' ORDER BY id ASC;
         """.format(search)
         cur.execute(stmt)
-        query_rows = cur.fetchall()
+        rtn = cur.fetchall()
     except MySQLdb.Error:
         try:
-            print ("MySQLdb Error")
+            rtn = ("MySQLdb Error")
         except IndexError:
-            print ("MySQLdb Error - IndexError")
-    for row in query_rows:
-        print(row)
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
     cur.close()
-    """ Close all cursors """
     conn.close()
-    """ Close all databases """
