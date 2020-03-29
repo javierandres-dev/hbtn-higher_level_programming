88aea2a23eb7fe1c4ff990412b84b593710d2a43
0x00developer <info@tecnoayuda.co>
2020-03-29 00:14:23 +0000

    task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
-"""
-    write one script that is safe from MySQL injections!
-"""
+""" write one script that is safe from MySQL injections! """
 from sys import argv
-""" module to use command line arguments """
 import MySQLdb
-""" module to connect to a MySQL database and execute SQL queries. """
 if __name__ == "__main__":
-    """ execute only if run as a sript """
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3],
-        charset="utf8"
-    )
+        charset="utf8")
     cur = conn.cursor()
     try:
         search = argv[4]
         stmt = """
-        SELECT * FROM states WHERE name = %s ORDER BY id ASC;
+        SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC
         """
         cur.execute(stmt, (search,))
-        query_rows = cur.fetchall()
+        rtn = cur.fetchall()
     except MySQLdb.Error:
         try:
-            print ("MySQLdb Error")
+            rtn = ("MySQLdb Error")
         except IndexError:
-            print ("MySQLdb Error - IndexError")
-    for row in query_rows:
-        print(row)
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
     cur.close()
-    """ Close all cursors """
     conn.close()
-    """ Close all databases """
