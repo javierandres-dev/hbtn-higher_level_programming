ac57f732a8d2a3a686b2c515b677a14ae6f85e46
0x00developer <info@tecnoayuda.co>
2020-04-09 20:59:05 -0500

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# cURL - displays the size
 curl -sD - "$1" | grep Content-Length | cut -d " " -f 2
