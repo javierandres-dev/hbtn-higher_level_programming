9e9ac9d150ef7bc1d0e350fdc01bd747d10e71a4
0x00developer <info@tecnoayuda.co>
2020-04-16 10:50:47 -0500

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import requests
     my_req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("    - type: {}".format(type(my_req.text)))
-    print("    - content: {}".format(my_req.text))
+    print("\t- type: {}".format(type(my_req.text)))
+    print("\t- content: {}".format(my_req.text))
