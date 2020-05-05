c07c5a06931b83be06213b6c43f6f74cecc7be2b
0x00developer <info@tecnoayuda.co>
2020-05-05 15:17:22 -0500

    Task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 // script that prints 3 lines
-let a = 'C is fun';
-console.log(a);
-let b = 'Python is cool';
-console.log(b);
-let c = 'Javascript is amazing';
-console.log(c);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
