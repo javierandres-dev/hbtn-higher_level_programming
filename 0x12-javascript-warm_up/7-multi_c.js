a68e6f465a2de804a4a1650af573a4416183e47a
0x00developer <info@tecnoayuda.co>
2020-05-05 17:08:06 -0500

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 // script that prints x times “C is fun”
 const x = process.argv[2];
 let text = 'C is fun';
-if (x === undefined) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
