d7d4b8c74636a6a8673f2e01cdf970c02d7e0ee8
0x00developer <info@tecnoayuda.co>
2020-05-05 17:25:55 -0500

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints a square
 const size = process.argv[2];
-if (isNaN(size)) {
+if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
