8f6402729a7365a978ddbb48129361b557101e51
0x00developer <info@tecnoayuda.co>
2020-05-05 17:38:51 -0500

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints the addition of 2 integers
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+function add (a, b) {
+  let result = a + b;
+  console.log(result);
+}
+add (a, b);
