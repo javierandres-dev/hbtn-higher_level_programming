d2a17f7ca37750b481f40b1be11f327e5ea027ed
0x00developer <info@tecnoayuda.co>
2020-05-05 20:36:18 -0500

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments
 const argv = process.argv;
-if (argv.length <= 3) {
+const size = argv.length;
+let i;
+let myArray = [];
+let biggest;
+let secondBiggest;
+if (size <= 3) {
   console.log(0);
 } else {
-  let myArray = [];
-  for (let i = 2; i < argv.length; i++) {
+  for (i = 2; i < size; i++) {
     myArray[i - 2] = parseInt(argv[i]);
   }
-  let biggest = Math.max.apply(null, myArray);
+  biggest = Math.max.apply(null, myArray);
   myArray.splice(myArray.indexOf(biggest), 1);
-  let secondBiggest = Math.max.apply(null, myArray);
+  secondBiggest = Math.max.apply(null, myArray);
   console.log(secondBiggest);
 }
