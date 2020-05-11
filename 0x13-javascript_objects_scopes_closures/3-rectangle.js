4637f8b03b8dd9613bf2d5aca1e2afdb224a50f6
0x00developer <info@tecnoayuda.co>
2020-05-11 14:39:58 -0500

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,11 +7,10 @@ class Rectangle {
       this.height = h;
     }
   }
-
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log("X".repeat(this.width));
     }
   }
 }
-module.exports = Rectangle
+module.exports = Rectangle;
