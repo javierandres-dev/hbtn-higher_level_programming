4f18bfa8ae62d3dd66090ad51bc5144ca56c224c
0x00developer <info@tecnoayuda.co>
2020-05-11 13:45:50 -0500

    Task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
 //  script that concats 2 files
+const fs = require('fs');
+const firstFile = fs.readFileSync(process.argv[2]).toString();
+const secondFile = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], (firstFile + secondFile));
