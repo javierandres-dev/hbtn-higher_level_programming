99f713752d9752633cdc4e3e9393cb77e2dc1f2f
0x00developer <info@tecnoayuda.co>
2020-05-11 13:23:12 -0500

    Task 11

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//  script that imports an array and computes a new array
+const list = require("./100-data").list;
+console.log(list);
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+console.log(newList);
