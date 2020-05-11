77d4f006f96f8f9eb794b8baf5d2fd5e9cf7f169
0x00developer <info@tecnoayuda.co>
2020-05-11 13:27:46 -0500

    Task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* script that imports a dictionary of occurrences by user id
+and computes a dictionary of user ids by occurrence */
+const dict = require("./101-data").dict;
+const newDict = {};
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);
