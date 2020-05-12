eb75c2a923f26d97aae085d20fa0a9152945a36a
0x00developer <info@tecnoayuda.co>
2020-05-12 17:33:05 -0500

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 /* script that prints the number of movies where
 the character “Wedge Antilles” is present. */
-const rtfm = 'The Force Awakens';
 const charId = 18;
 const url = 'https://swapi-api.hbtn.io/api/films/';
 require('request').get(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else if (response.statusCode === 200) {
+  } else {
     const obj = JSON.parse(body);
     let times = 0;
     for (let i of obj.results) {
       for (let j of i.characters) {
-        if (j.search(18) > 0) {
-          times += 1;
+        if (j.search(charId) > 0) {
+          times++;
         }
       }
     }
     console.log(times);
-  } else {
-    console.log(rtfm);
   }
 });
