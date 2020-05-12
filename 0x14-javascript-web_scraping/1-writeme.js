68e9454e4ee778028e70cdfd321417901582610b
0x00developer <info@tecnoayuda.co>
2020-05-12 12:18:27 -0500

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 //  script that writes a string to a file.
 let fPath = process.argv[2];
-let content = process.argv[3];
+let toWrite = process.argv[3];
 const fs = require('fs');
-fs.writeFile(fPath, content, 'utf8', function (err) {
+fs.writeFile(fPath, toWrite, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
