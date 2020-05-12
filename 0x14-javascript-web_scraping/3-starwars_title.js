edd6fc14d0cc61e0ad783cb04231b2d616ae6ea5
0x00developer <info@tecnoayuda.co>
2020-05-12 13:04:44 -0500

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,8 @@ const id = process.argv[2];
 const apiUrl = 'http://swapi-api.hbtn.io/api/films/';
 const endpoint = apiUrl + id;
 const req = require('request');
-req.get(endpoint, function (error, response, data) {
-  if (error == null) {
+req(endpoint, function (err, res, data) {
+  if (err == null) {
     const obj = JSON.parse(data);
     console.log(obj.title);
   }
