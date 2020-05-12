ed93cd59f649fa5a58c79be987e6670e55e06f8d
0x00developer <info@tecnoayuda.co>
2020-05-12 18:09:41 -0500

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const process = require('process');
 const fs = require('fs');
-request(process.argv[2], function (error, response, body){
+request(process.argv[2], function (error, response, body) {
   if (error == null) {
     fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
       if (err) {
