2c25d3e1e685921d2e723ff4bbed5f1274f6a342
0x00developer <info@tecnoayuda.co>
2020-05-12 10:00:58 -0500

    Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that display the status code of a GET request.
+const url = process.argv[2];
+const req = require('request');
+req(url, function (err, res) {
+  if (err) {
+    console.error('code: ', err);
+  } else {
+    console.log('code: ', res.statusCode);
+  }
+});
