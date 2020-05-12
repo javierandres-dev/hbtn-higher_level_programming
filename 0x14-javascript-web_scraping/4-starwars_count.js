74c2543b6b20b44652242121eefcb39f3d4ba0c4
0x00developer <info@tecnoayuda.co>
2020-05-12 16:10:53 -0500

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,9 @@ require('request').get(apiUrl, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const obj = JSON.parse(body).results;
+    const obj = JSON.parse(body);
     let times = 0;
-    for (let i of obj) {
+    for (let i of obj.results) {
       for (let j of i.characters) {
         if (j.indexOf('/18/') > 0) {
           times += 1;
