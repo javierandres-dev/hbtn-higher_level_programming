8d77a1b2f5bee0a91c6cad048ed3ca98cc4b136d
0x00developer <info@tecnoayuda.co>
2020-05-12 17:51:29 -0500

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 /* script that prints the number of movies where
 the character “Wedge Antilles” is present. */
-const charId = '18';
-const request = require('request')
-request(process.argv.slice(2)[0], function (error, response, body) {
+const request = require('request');
+request(process.argv.slice(2)[0], function (error, body) {
   if (error) {
-    console.log(error);
+    console.error('error:', error);
   } else {
-    const obj = JSON.parse(body);
     let times = 0;
-    for (let i of obj.results) {
-      for (let j of i.characters) {
-        if (j.includes(charId)) {
+    for (const i of JSON.parse(body.body).results) {
+      const character = i.characters;
+      for (const j of character) {
+        if (j.includes('18')) {
           times += 1;
         }
       }
