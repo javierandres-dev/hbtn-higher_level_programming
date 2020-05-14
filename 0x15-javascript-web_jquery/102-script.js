676f461f02a8f2fb90489973a67cacc8ae5abf74
0x00developer <info@tecnoayuda.co>
2020-05-14 14:09:09 -0500

    Task 12

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,9 +1,9 @@
 const $ = window.$;
 window.onload = function () {
   $('INPUT#btn_translate').click(function () {
-    const lan = $('INPUT#language_code').val();
-    $.get('https://fourtonfish.com/hellosalut/?lang=' + lan, function (data, textStatus) {
-      $('DIV#hello').text(data.hello);
+    const language = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + language, function (say, textStatus) {
+      $('DIV#hello').text(say.hello);
     });
   });
 };
